4a50836d52f84a4f1e66dbb9091cd5668221c41e
Jeffree Berrocal <berrocal.97@hotmail.com>
2026-05-23 21:34:38 -0600

    3rd updade

--- pawstore/src/components/ProductDetail.jsx
+++ pawstore/src/components/ProductDetail.jsx
@@ -1,15 +1,20 @@
 function ProductDetail({ producto, setPagina, setProducto }) {
   return (
-    <main>
-      <img src={producto.imagen} alt={producto.nombre} />
-      <h2> {producto.nombre} </h2>
-      <p> {producto.precio} </p>
-      <p> {producto.categoria} </p>
-      <p> {producto.descripcion} </p>
-      <p>Más adelante aquí se podrá agregar este producto al carrito y completar la compra.</p>
-      <button onClick={ ()=>{
+    <main className="detalle">
+      <div className="detalle-imagen">
+        <img src={producto.imagen} alt={producto.nombre} />
+      </div>
+      <div className="detalle-info">
+        <h2> {producto.nombre} </h2>
+        <p> {producto.precio} </p>
+        <p> {producto.categoria} </p>
+        <p> {producto.descripcion} </p>
+        <p>Más adelante aquí se podrá agregar este producto al carrito y completar la compra.</p>
+        <button onClick={ ()=>{
         setPagina("catalogo")
       }} >Volver al Catálogo</button>
+      </div>
+      
     </main>
   )
 }
